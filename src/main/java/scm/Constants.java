67bbaf5cd6e8e0af5e41b9c99122cdf037f75517
Andrei Jause <andreigarciacuadra@gmail.com>
2018-07-16 21:27:14 +0200

    Prod/dev envs

--- src/main/java/scm/Constants.java
+++ src/main/java/scm/Constants.java
@@ -11,6 +11,7 @@ public class Constants
     public static final String apiUrl = "https://api.discoexample.dev.scmnight.com/app/";
     public static String NETWORK_MAC;
     public static String NETWORK_IP;
+    public static boolean IS_DEV = false;
     public static String APP_VERSION = "0.4.0";
     public static boolean IS_VIP_READER = false;
 }
